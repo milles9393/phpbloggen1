cbcb79fd5f97c8058297baf7a1a737a5e1527960
milles9393 <milletarp@live.se>
2020-08-12 23:23:42 +0200

    added form input

---
+++ shared/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    &copy; <?php echo date('Y'); ?> bloggen
+</footer>
+
+</body>
+</html>
---
+++ shared/header.php
@@ -0,0 +1,23 @@
+<?php
+if(!isset($page_title)) { $page_title = 'Staff Area'; }
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+    <title>GBI - </title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/header_footer.css" />
+</head>
+
+<body>
+<header>
+    <h1>GBI Staff Area</h1>
+</header>
+
+<navigation>
+    <ul>
+        <li><a href="../frontend/index.php">Menu</a></li>
+    </ul>
+</navigation>
